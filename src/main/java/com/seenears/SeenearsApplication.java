f92fbec9c8231795606f1680160c0de86f572dd0
geewon <wldnjs9515@naver.com>
2026-06-16 01:29:00 +0900

    chore: 스프링부트 프로젝트 초기 설정

---
+++ src/main/java/com/seenears/SeenearsApplication.java
@@ -0,0 +1,13 @@
+package com.seenears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeenearsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeenearsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seenears/SeenearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seenears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeenearsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
